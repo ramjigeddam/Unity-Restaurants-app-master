fe5581876ec62b276e034794355b1725596962ba
Ramji <ramjigeddam@gmail.com>
2019-12-24 16:37:21 +0530

    Added Drag and Rotation

---
+++ Assets/_Scripts/DragAndRotate/DragObject.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragObject : MonoBehaviour
+{
+    public GameObject player;
+    private bool isDragging = false;
+    private Ray ray;
+    private RaycastHit raycastHit;
+
+
+    private void Update()
+    {
+
+        int touches = Input.touchCount;
+
+        if (touches > 0 && touches != 2)
+        {
+            for (int i = 0; i < touches; i++)
+            {
+                Touch touch = Input.GetTouch(i);
+
+                TouchPhase phase = touch.phase;
+
+                switch (phase)
+                {
+                    case TouchPhase.Began:
+
+                        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                        if (Physics.Raycast(ray, out raycastHit))
+                        {
+                            if (raycastHit.collider.gameObject.transform.tag == "Model")
+                            {
+                                player = GameObject.FindGameObjectWithTag("Model");
+                            }
+                        }
+                        break;
+                    case TouchPhase.Moved:
+                        if (player != null)
+                        {
+                            float distanceToScreen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+                            Vector3 posMove = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, distanceToScreen));
+                            player.transform.position = posMove;
+                        }
+                        break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/_Scripts/DragAndRotate/RotateObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Quaternion desiredRotation = transform.rotation;
+
+        DetectTouchMovement.Calculate();
+
+        if (Mathf.Abs(DetectTouchMovement.turnAngleDelta) > 0)
+        { // rotate
+            Debug.Log("Rotate");
+            Vector3 rotationDeg = Vector3.zero;
+            rotationDeg.y = -DetectTouchMovement.turnAngleDelta;
+            desiredRotation *= Quaternion.Euler(rotationDeg);
+
+        }
+
+
+        // not so sure those will work:
+        transform.rotation = desiredRotation;
+        //transform.position += Vector3.forward * pinchAmount;
+    }
+}
